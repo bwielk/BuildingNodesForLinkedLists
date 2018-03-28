77c81d50853a38a4be9291569fe189981a6ca1d9
bwielk <bwielk@gmail.com>
2018-03-28 23:36:07 +0200

    Testing the compareTo() method

--- tests/nodes/LinkedListTest.java
+++ tests/nodes/LinkedListTest.java
@@ -67,4 +67,24 @@ public class LinkedListTest {
 		assertEquals("Naomi", list.getRoot().next().getValue());
 		assertEquals("Julia", list.getRoot().next().previous().getValue());
 	}
+	
+	@Test
+	public void compareToMethodWorksWhenWord1IsLessThanWord2(){
+		ListItem word1 = item2;//Naomi
+		ListItem word2 = item1;//Julia
+		assertEquals(0, word1.compareTo(word1));
+		assertEquals(true, word1.compareTo(word2) > 0);//ASCII value > 0
+	}
+	
+	@Test
+	public void compareToMethodWorksWhenWord1IsGreaterThanWord2(){
+		ListItem word1 = item1;//Anais
+		ListItem word2 = item3;//Julia
+		assertNotEquals(true, word1.compareTo(word2) > 0);
+		assertNotEquals(true, word2.compareTo(word1) < 0);
+		assertEquals(true, word1.compareTo(word2) < 0);
+	}
+	
+	
+	
 }
