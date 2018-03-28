c0c1607d3f7dd2c87c60169ade3636289c20050b
bwielk <bwielk@gmail.com>
2018-03-28 22:51:40 +0200

    Added more tests for addItem in LinkedList

--- tests/nodes/LinkedListTest.java
+++ tests/nodes/LinkedListTest.java
@@ -24,6 +24,26 @@ public class LinkedListTest {
 	public void listDoesntHaveAnyNodesInside(){
 		assertNull(list.getRoot());
 	}
-
-
+	
+	@Test
+	public void listWithoutNodesCreatesRootOnceAcceptANewItem(){
+		assertNull(list.getRoot());
+		assertEquals(true, list.addItem(item1));
+		assertEquals("Anais", list.getRoot().getValue());
+		assertEquals(null, list.getRoot().next());
+	}
+	
+	@Test
+	public void listWithoutNodesCreatesRootAndReferencesToPreviousAndNextItems(){
+		assertEquals(true, list.addItem(item1));
+		assertEquals(true, list.addItem(item2));
+		assertEquals("Anais", list.getRoot().getValue());
+		assertNull(null, list.getRoot().previous());
+		assertEquals("Naomi", list.getRoot().next().getValue());
+		ListItem naomi = list.getRoot().next();
+		assertNull(null, naomi.next());
+	}
+	
+	@Test
+	public void 
 }
